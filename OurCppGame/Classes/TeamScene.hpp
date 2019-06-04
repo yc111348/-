0bd20f8ac0567f06542a1329a41674521b162ae9
Dzzzy <44988791+Dzzzy@users.noreply.github.com>
2019-06-04 08:01:11 +0800

    联机模式界面(待完成)

---
+++ OurCppGame/Classes/TeamScene.hpp
@@ -0,0 +1,28 @@
+//
+//  TeamScene.hpp
+//  OurCppGame
+//
+//  Created by 董震宇 on 2019/6/3.
+//
+
+#ifndef TeamScene_hpp
+#define TeamScene_hpp
+
+#include <stdio.h>
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class TeamScene: public Layer
+{
+    
+public:
+    
+    virtual bool init();
+    static Scene* createScene();
+    CREATE_FUNC(TeamScene);
+    
+    void menuGoToSCCScene(cocos2d::Ref * Sender);
+};
+
+#endif /* TeamScene_hpp */
