a82b1d7e707305f52d781b91f09cc0d65686d736
yc111348 <405458065@qq.com>
2019-06-15 12:59:44 +0800

    怪物刷新

--- helloworld/Classes/Heroscene.cpp
+++ helloworld/Classes/Heroscene.cpp
@@ -607,9 +607,11 @@ void Hero::enemycoming()
     enemy->runAction(RepeatForever::create(Animate::create(enemy_animation1))); //测试动画
     enemylive=1;
     enemyblood=100;
+    enemy_x = enemy->getPositionX();
+    enemy_y=enemy->getPositionY();
     enemy_pProgressView = new ProgressView();
     enemy_pProgressView->setScale(1,0.5);
-    enemy_pProgressView->setPosition(enemy_x+330,enemy_y+450);
+    enemy_pProgressView->setPosition(330,450);
     enemy_pProgressView->setBackgroundTexture("item/noblood.png");
     enemy_pProgressView->setForegroundTexture("item/fullblood.png");
     enemy_pProgressView->setTotalProgress(100.0f);
@@ -1540,6 +1542,8 @@ void Hero::enemyforup()
     if(time==enemytime&&!enemylive)
     {
         enemycoming();
+        enemymovetime=enemytime;
+        enemyacktime=enemytime;
         enemytime+=1200;
     }
     if(enemylive)
