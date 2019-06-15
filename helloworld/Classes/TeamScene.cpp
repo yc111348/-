d4ac844d84dec269c348a44ea59aa36c85ebe749
Dzzzy <315300270@qq.com>
2019-06-16 02:02:26 +0800

    完善团队界面

--- helloworld/Classes/TeamScene.cpp
+++ helloworld/Classes/TeamScene.cpp
@@ -37,12 +37,20 @@ bool TeamScene::init()
     
     
     
+    auto hint = Label::createWithTTF("暂未开发敬请期待！", "fonts/chicken.ttf", 60);
+    hint -> setPosition(Vec2(size.width/2,size.height*9/11));
+    hint -> setColor(Color3B(54,54,54));
+    addChild(hint,3);
     
+    auto hint2 = Label::createWithTTF("可以赞助我们哦~", "fonts/chicken.ttf", 60);
+    hint2 -> setPosition(Vec2(size.width/2,size.height*9/11 - 120));
+    hint2 -> setColor(Color3B(54,54,54));
+    addChild(hint2,3);
     
-    
-    
-    
-    
+    auto pay = Sprite::create("UIres/pay.png");
+    pay -> setPosition(Vec2(size.width/2,size.height/6));
+    pay -> setScale(0.35, 0.35);
+    addChild(pay,3);
     
     
     return true;
